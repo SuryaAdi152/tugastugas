86a53a52911e4f9517603b4c9f2c029c3ed40e1d
SuryaAdi152 <114859732+SuryaAdi152@users.noreply.github.com>
2023-10-12 23:37:18 +0800

    Update aksi.php

--- aksi.php
+++ aksi.php
@@ -4,35 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hasil Formulir Biodata</title>
-<style>
-    body {
-        font-family: Arial, sans-serif;
-        background-image: url('hallow.gif'); /* Ganti 'halloween_bg.gif' dengan nama file gambar Anda */
-        background-size: cover;
-        background-attachment: fixed;
-        background-repeat: no-repeat;
-        text-align: center;
-    }
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-image: url('hallow.gif'); /* Ganti 'halloween_bg.gif' dengan nama file gambar Anda */
+            background-size: cover;
+            background-attachment: fixed;
+            background-repeat: no-repeat;
+            text-align: center;
+        }
 
-    .container {
-        background-color: rgba(255, 255, 255, 0.7);
-        border-radius: 8px;
-        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-        max-width: 400px;
-        margin: 20px auto;
-        padding: 20px;
-    }
+        .container {
+            background-color: rgba(255, 255, 255, 0.7);
+            border-radius: 8px;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+            max-width: 400px;
+            margin: 20px auto;
+            padding: 20px;
+        }
 
-    h1 {
-        color: #FF4500;
-    }
+        h1 {
+            color: #FF4500;
+        }
 
-    p {
-        text-align: left;
-        margin: 5px 0;
-    }
-</style>
-
+        p {
+            text-align: left;
+            margin: 5px 0;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
@@ -43,7 +42,7 @@
             $alamat = $_POST["alamat"];
             $jenis_kelamin = $_POST["jenis_kelamin"];
             $jurusan = $_POST["jurusan"];
-            $Hobi = $_POST["Hobi"];
+            $hobi = $_POST["hobi"];  // Mengubah "Hobi" menjadi "hobi"
             $email = $_POST["email"];
 
             echo "<h1>Hasil Formulir Biodata</h1>";
@@ -52,7 +51,7 @@
             echo "<p><strong>Alamat:</strong> $alamat</p>";
             echo "<p><strong>Jenis Kelamin:</strong> $jenis_kelamin</p>";
             echo "<p><strong>Jurusan:</strong> $jurusan</p>";
-            echo "<p><strong>Hobi:</strong> $Hobi</p>";
+            echo "<p><strong>Hobi:</strong> $hobi</p>";  // Mengubah "Hobi" menjadi "hobi"
             echo "<p><strong>Email:</strong> $email</p>";
         } else {
             echo "<p>Terjadi kesalahan dalam pengiriman formulir.</p>";
